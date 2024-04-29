64500abea3b02584fb504327fab9fb90e905892e
Bin2y <37493119+Bin2y@users.noreply.github.com>
2024-04-29 17:43:48 +0900

    [feat] : Func - TakeDamage& 3monsters.cs
    3개의 몬스터 스크립트와 TakeDamage 메서드 작성

--- SpartaDungeonBattle/Monster.cs
+++ SpartaDungeonBattle/Monster.cs
@@ -9,16 +9,34 @@ namespace SpartaDungeonBattle
 {
     public class Monster
     {
-        private string name { get; set; }
-        private int lv {  get; set; }
-        private int hp { get; set; }
+        public string Name { get; set; }
+        public int Lv { get; set; }
+        public int Hp { get; set; }
+        public int AttackPower { get; set; }
+        public bool IsDead { get; set; }
 
-        public Monster(string name, int lv, int hp)
+        public Monster(string name, int lv, int attackPower, int hp)
         {
-            this.name = name;
-            this.lv = lv;
-            this.hp = hp;
+            Name = name;
+            Lv = lv;
+            Hp = hp;
+            AttackPower = attackPower;
+            IsDead = false;
         }
 
+        void TakeDamage(int damage)
+        {
+            if (IsDead)
+            {
+                Console.WriteLine("이미 죽은 몬스터입니다.");
+                return;
+            }.
+            else
+            {
+                Hp -= damage;
+                if (IsDead) Console.WriteLine($"{Name}이(가) 죽었습니다.");
+                else Console.WriteLine($"{Name}이(가) {damage}만큼 피해를 입었습니다. 남은 체력 {Hp}");
+            }
+        }
     }
 }
