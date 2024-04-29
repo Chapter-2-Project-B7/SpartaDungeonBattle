06e6f39d82db5ab5d089f2848890c54b3aa79e02
MothorMoth <mothormoth@gmail.com>
2024-04-29 12:35:25 +0900

    Feat: Initialize GameManager class setup

---
+++ SpartaDungeonBattle/GameManager.cs
@@ -0,0 +1,19 @@
+﻿namespace SpartaDungeonBattle
+{
+    public class GameManager
+    {
+        public GameManager() { }
+
+        private void InitializeGame() { }
+
+        public void StartGame() { }
+
+        private void EndGame() { }
+
+        private void ShowMainMenu() { }
+
+        private void ShowInventoryMenu() { }
+
+        private void ShowBattleMenu() { }
+    }
+}
