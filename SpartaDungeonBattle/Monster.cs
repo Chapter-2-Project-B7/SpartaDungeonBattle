8582aa47f693e3c385d3858cad0ca71981c40183
Bin2y <37493119+Bin2y@users.noreply.github.com>
2024-04-29 12:17:28 +0900

    [Feat]: Make Monster Field

---
+++ SpartaDungeonBattle/Monster.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpartaDungeonBattle
+{
+    public class Monster
+    {
+        private string name { get; set; }
+        private int lv {  get; set; }
+        private int hp { get; set; }
+
+        public Monster(string name, int lv, int hp)
+        {
+            this.name = name;
+            this.lv = lv;
+            this.hp = hp;
+        }
+
+    }
+}
