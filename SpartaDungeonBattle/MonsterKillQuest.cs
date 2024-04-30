71a05623612d571ce30c9ac8bf593e71efcfabab
Bin2y <37493119+Bin2y@users.noreply.github.com>
2024-04-30 10:04:57 +0900

    feat : Quest, MonsterKillQuest
    1. 퀘스트 추상 클래스 작성
    2. 퀘스트를 상속받는 MonsterKillQuest작성

---
+++ SpartaDungeonBattle/MonsterKillQuest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpartaDungeonBattle
+{
+    public class MonsterKillQuest : Quest
+    {
+        public MonsterKillQuest() : base(
+                                    "마을을 위협하는 몬스터 처치",
+                                    "이봐! 마을 근처에 몬스터들이 너무 많아졌다고 생각하지 않나?\n" +
+                                    "마을주민들의 안전을 위해서라도 저것들 수를 좀 줄여야 한다고!\n" +
+                                    "모험가인 자네가 좀 처치해주게!",
+                                    "쓸만한 방패"){}
+
+        override public void Request()
+        {
+            Console.WriteLine("- 슬라임 5마리 처치 (0/5)");
+        }
+    }
+}
---
+++ SpartaDungeonBattle/Quest.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpartaDungeonBattle
+{
+    abstract public class Quest
+    {
+        
+        public string Name { get; set; }
+        //퀘스트 설명
+        public string Description { get; set; }
+        //아이템 클래스 생성하면 보상을 아이템으로 변경하도록 만들 수 있다.
+        public string Reward { get; set; }
+        public bool IsComplete { get; set; }
+
+        public Quest(string name, string description, string reward)
+        {
+            Name = name;
+            Description = description;
+            Reward = reward;
+            IsComplete = false;
+        }
+
+        public void RecieveQuest()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Quest!!");
+            Console.WriteLine();
+            Console.WriteLine(Name);
+            Console.WriteLine();
+            Console.WriteLine(Description);
+            Console.WriteLine();
+            //Request는 자식 클래스에서 작성후 부르면됨
+            Request();
+            Console.WriteLine();
+            Console.WriteLine("-보상-");
+            Console.WriteLine(Reward); 
+            Console.WriteLine();
+            Console.WriteLine("1. 수락");
+            Console.WriteLine("2. 거절");
+            Console.WriteLine("원하시는 행동을 입력해주세요.");
+
+        }
+
+        //퀘스트 요구사항은 자식들이 오버라이딩 할 수 있도록
+        abstract public void Request();
+
+
+        public void ClearQuest()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Quest!!");
+            Console.WriteLine();
+            Console.WriteLine(Name);
+            Console.WriteLine();
+            Console.WriteLine(Description);
+            Console.WriteLine();
+            Request();
+            Console.WriteLine();
+            Console.WriteLine("-보상-");
+            Console.WriteLine(Reward);
+            Console.WriteLine();
+            Console.WriteLine("1. 보상 받기");
+            Console.WriteLine("2. 돌아가기");
+            Console.WriteLine("원하시는 행동을 입력해주세요.");
+        }
+    }
+}
