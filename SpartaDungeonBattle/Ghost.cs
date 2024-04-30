98b2bc77301053126e52d2f05baff252f66cd9aa
Bin2y <37493119+Bin2y@users.noreply.github.com>
2024-04-30 16:32:17 +0900

    Remove : Ghost.cs,Golem.cs,Slime.cs

--- SpartaDungeonBattle/Ghost.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SpartaDungeonBattle
-{
-    public class Ghost : Monster
-    {
-        public Ghost()
-            : base(6, "Ghost", 15, 15) { }
-    }
-}
--- SpartaDungeonBattle/Golem.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SpartaDungeonBattle
-{
-    public class Golem : Monster
-    {
-        public Golem()
-            : base(4, "Golem", 8, 20) { }
-    }
-}
