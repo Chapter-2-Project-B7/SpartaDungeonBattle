1bb479f5ebfa4cbbb1d8b627ed45d0c0a34ddaad
MothorMoth <mothormoth@gmail.com>
2024-05-01 17:30:11 +0900

    Refactor: Fix startHp in ShowVictoryResult method

--- SpartaDungeonBattle/GameManager.cs
+++ SpartaDungeonBattle/GameManager.cs
@@ -62,6 +62,7 @@
         internal List<Item> clearItemList;
 
         private Random rand;
+        private int startHP;
 
         public GameManager()
         {
@@ -260,6 +261,8 @@
             //클리어아이템 받을 리스트를 초기화
             clearItemList.Clear();
 
+            startHP = player.HealthPoint;
+
             Console.Clear();
             ConsoleUtility.ShowTitle("Battle!!");
             Console.WriteLine();
@@ -438,7 +441,7 @@
             player.GetExp(gainEXP);
             Console.WriteLine();
             Console.Write("HP ");
-            ConsoleUtility.PrintTextSectionsHighlights($"100", " -> ", $"{player.HealthPoint}");
+            ConsoleUtility.PrintTextSectionsHighlights($"{startHP}", " -> ", $"{player.HealthPoint}");
             Console.Write("exp ");
             ConsoleUtility.PrintTextSectionsHighlights($"{currentEXP}", " -> ", $"{currentEXP + gainEXP}");
             Console.WriteLine();
