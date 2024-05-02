f828984adc41ee75801428fb4814063e7239eace
Bin2y <37493119+Bin2y@users.noreply.github.com>
2024-05-02 12:40:43 +0900

    Refactor - SaveLoad Method
    주석 제거

--- SpartaDungeonBattle/Program.cs
+++ SpartaDungeonBattle/Program.cs
@@ -7,25 +7,6 @@ namespace SpartaDungeonBattle
         static void Main()
         {
             GameManager.Instance.StartGame();
-            
-            //직렬화 테스트 코드
-            /*QuestManager.Instance.InitQuest();
-            
-
-            
-            string fileName = "Quests.json";
-            var options = new JsonSerializerOptions { WriteIndented = true };
-            Monster molnster = new Monster(1, "Slime", 0, 0);
-            molnster.Die();
-            QuestManager.Instance.EnterQuest();
-            MonsterKillQuest? killQuest = QuestManager.Instance.quests[0] as MonsterKillQuest;
-            string? jsonString = JsonSerializer.Serialize<MonsterKillQuest>(killQuest, options);
-
-            //string jsonString = File.ReadAllText(fileName);
-
-            //Quest? quest = JsonSerializer.Deserialize<Quest>(jsonString);
-            Console.Write(jsonString);
-        */
         }
     }
 }
